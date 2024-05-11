35075a693dff1156ef71849585efe8df4c882dcb
L50six <137829522+L50six@users.noreply.github.com>
2024-05-11 12:02:38 +0200

    Update ch6_horseplay.cpp
    Main code before refinemenrs

--- src/06/Challenge/ch6_horseplay.cpp
+++ src/06/Challenge/ch6_horseplay.cpp
@@ -25,9 +25,23 @@ std::vector<std::string> knight_moves(std::string knight){
 	}
 
 	vector<KnightMove> KnightMoves = {
-		{1,2},{-1,2},{-1,-2}, {-1,2},
+		{1,2},{-1,2},{-1,-2}, {1,-2},
 		{2,1},{-2,1},{-2,-1},{2,-1}
     };
+
+	x = static_cast<int>(knight[[0]);
+	y = static_cast<int>(knight[[1]);
+
+	//  needs assci offset
+
+	for (const KnightMove& move: KnightMoves) {
+		x += move.dx;
+		y += move.dy;
+		if ((x > 0) && (x<9) && (y > 0) && (y<9)) {
+			moves.push_back(std::itos(x),std::itos(y))
+			// columns need to be converrted to letterw. 
+		}
+	}
 
     return moves;
 }
